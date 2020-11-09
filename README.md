a119ef697082ed50cc9efb35a1ac012016e675fa
GwenStacey <gwen.c.purcell@gmail.com>
2020-11-09 08:00:11 -0700

    added simple add function, doesn't work for more than two numbers.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # simple_calc
 Simple calculator, made to practice basic python.
+
+# Usage
+In this section, I'll describe using the calculator as soon as the basic functions have been implemented.
---
+++ simple_calc.py
@@ -0,0 +1,16 @@
+"""This is a simple calculator. It will at the very least, add, subtract,
+multiply, and divide. I may at some later point, add more complicated math
+functions, but this basic implementation will just so some simple math."""
+
+def add(a,b):
+    """This function will add two numbers, a b, together"""
+    return a+b
+
+while True:
+    print("Hello, what math can I do for you today?")
+    selection = input("What math function should I run?")
+    num1 = int(input("What's the first number I should use?"))
+    num2 = int(input("What's the second number I should use?"))
+    if selection == "add":
+        print(add(num1, num2))
+        
