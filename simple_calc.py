ee9729d657bbcbb687b20a3fce804c4352b949de
GwenStacey <gwen.c.purcell@gmail.com>
2020-11-10 15:33:08 -0700

    Added subtraction function and end command

--- simple_calc.py
+++ simple_calc.py
@@ -6,11 +6,19 @@ def add(a,b):
     """This function will add two numbers, a b, together"""
     return a+b
 
+def subtract(a,b):
+    return a-b
+
 while True:
     print("Hello, what math can I do for you today?")
     selection = input("What math function should I run?")
+    if selection == "end":
+        break
     num1 = int(input("What's the first number I should use?"))
     num2 = int(input("What's the second number I should use?"))
     if selection == "add":
         print(add(num1, num2))
+    if selection =="subtract":
+        print(subtract(num1, num2))
+    
         
